77ab8c7593716db1893c85faf09bc1a645539801
AJoelj84 <ajoelj84@gmail.com>
2023-05-15 13:38:17 -0400

    added put route to category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
     res.status(200).json(productCategory);
   }
     catch (err){
-      res.status(500).json(err);
+    res.status(500).json(err);
     }
   // find all categories
   // be sure to include its associated Products
@@ -28,7 +28,7 @@ router.get('/:id', async (req, res) => {
     }
     res.status(200).json(productCategory);
   }
-  catch(err){
+    catch(err){
     res.status(500).json(err);
   }
   // find one category by its `id` value
@@ -40,13 +40,23 @@ router.post('/', async (req, res) => {
     const productCategory = await Category.create(req.body);
     res.status(200).json(productCategory);
   }
-  catch(err){
+    catch(err){
     res.status(400).json(err);
   }
   // create a new category
 });
 
 router.put('/:id', async (req, res) => {
+  try {
+    const productCategory = await Category.update(
+      {category_name: req.body.category_name},
+      {where: req.params.id}
+    );
+    res.status(200).json(productCategory);
+  } 
+    catch (err) {
+    res.status(400).json(err);
+  }
   // update a category by its `id` value
 });
 
